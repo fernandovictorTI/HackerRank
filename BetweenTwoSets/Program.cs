004ae313919c4f6360f40ebe89a2617dc7a5f869
unknown <fernandovictorti@gmail.com>
2021-04-02 03:54:49 -0400

    Between two sets

---
+++ BetweenTwoSets/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BetweenTwoSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetTotalX(new List<int> { 2, 4 }, new List<int> { 16, 32, 96 });
+        }
+
+        public static int GetTotalX(List<int> a, List<int> b)
+        {
+            var totalXs = 0;
+            var maximumA = a.Max();
+            var minimumB = b.Min();
+            var counter = 1;
+            var multipleOfMaxA = maximumA;
+
+            while (multipleOfMaxA <= minimumB)
+            {
+                var factorOfAll = true;
+
+                foreach (var item in a)
+                {
+                    if (multipleOfMaxA % item != 0)
+                        factorOfAll = false;
+                }
+
+                if (factorOfAll)
+                {
+                    foreach (var item in b)
+                    {
+                        if (item % multipleOfMaxA != 0)
+                        {
+                            factorOfAll = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (factorOfAll)
+                    totalXs++;
+
+                counter++;
+                multipleOfMaxA = maximumA * counter;
+            }
+
+            return totalXs;
+        }
+    }
+}
