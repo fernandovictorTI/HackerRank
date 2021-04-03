1fe688e05268f0a3e98ea20e83f268ebe3aed9c7
unknown <fernandovictorti@gmail.com>
2021-04-03 00:23:14 -0400

    Divisible num pairs e migratory birds

---
+++ DivisibleNumPairs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DivisibleNumPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DivisibleSumPairs(6, 3, new int[] { 1, 3, 2, 6, 1, 2 })); // Output 5
+        }
+
+        public static int DivisibleSumPairs(int n, int k, int[] ar)
+        {
+            var contador = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i; j < n - 1; j++)
+                {
+                    if ((ar[i] + ar[j + 1]) % k == 0)
+                        contador++;
+                }
+            }
+
+            return contador;
+        }
+    }
+}
---
+++ MigratoryBirds/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MigratoryBirds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FnMigratoryBirds(new List<int>() { 1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4 })); // Output 3
+            Console.WriteLine(FnMigratoryBirds(new List<int>() { 1, 4, 4, 4, 5, 3 })); // Output 4
+        }
+
+        public static int FnMigratoryBirds(List<int> arr)
+        {
+            var groupd = arr.GroupBy(c=> c);
+            var indiceMaior = groupd
+                                .Where(c=> c.Count() == groupd.Max(d=> d.Count()))
+                                .OrderBy(c=> c.Key)
+                                .Select(c=> c.Key)
+                                .FirstOrDefault();                      
+
+            return indiceMaior;
+        }
+    }
+}
