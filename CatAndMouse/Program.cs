ec111c15038222d86e26892734bc412de593c721
Fernando Victor <fernandovictorti@gmail.com>
2022-03-31 21:24:39 -0400

    CatAndMouse

---
+++ CatAndMouse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CatAndMouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(catAndMouse(2, 5, 4));
+        }
+
+        // Complete the catAndMouse function below.
+        static string catAndMouse(int x, int y, int z)
+        {
+            int quantidadeVezesGatoA = Math.Abs(x - z);
+            int quantidadeVezesGatoB = Math.Abs(y - z);
+
+            if (quantidadeVezesGatoA == quantidadeVezesGatoB)
+            {
+                return "Mouse C";
+            }
+            else if (quantidadeVezesGatoA > quantidadeVezesGatoB)
+            {
+                return "Cat B";
+            }
+            else
+            {
+                return "Cat A";
+            }
+        }
+    }
+}
