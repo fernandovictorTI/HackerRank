1915e2b5e51af89cfc3ef5130cebf69c41e421b3
Fernando Victor <fernandovictorti@gmail.com>
2022-04-05 00:03:49 -0400

    ExtraLongFactorials

---
+++ ExtraLongFactorials/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+
+namespace ExtraLongFactorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            extraLongFactorials(25);
+        }
+
+        /*
+     * Complete the 'extraLongFactorials' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+        public static void extraLongFactorials(int n)
+        {
+            BigInteger resuldado = n;
+            for (int i = n - 1; i > 0; i--)
+            {
+                resuldado = resuldado * i;
+            }
+
+            Console.WriteLine(resuldado);
+        }
+    }
+}
