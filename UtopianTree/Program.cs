a2bb0975ad1f765657122b33e1ea92ff512d0db1
Fernando Victor <fernandovictorti@gmail.com>
2022-04-05 23:26:16 -0400

    utopianTree

---
+++ UtopianTree/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace UtopianTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(UtopianTree(5));
+        }
+
+        /*
+        * Complete the 'utopianTree' function below.
+        *
+        * The function is expected to return an INTEGER.
+        * The function accepts INTEGER n as parameter.
+        */
+        public static int UtopianTree(int n)
+        {
+            if (n == 0) return 1;
+            if (n == 1) return 2;
+
+            int upT = 2;
+
+            for (int i = 2; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                    upT += 1;
+                else
+                {
+                    upT *= 2;
+                }
+            }
+
+            return upT;
+        }
+    }
+}
